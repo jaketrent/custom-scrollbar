bba6d78329339a025fb4245a18899bc93d0d230c
jaketrent <trent.jake@gmail.com>
2017-03-09 10:56:36 -0700

    scroll bar in proportion with frame, which also scrolls

--- scripts.js
+++ scripts.js
@@ -32,17 +32,10 @@ document.addEventListener('DOMContentLoaded', _ => {
 
   renderHeight(handle, calcHandleHeight(frame.height, content.height, bar.height))
 
+  console.log('frame height', frame.height)
+  console.log('content height', content.height)
 
-  // Reference: http://www.html5rocks.com/en/tutorials/speed/animations/
-
-  var lastKnownScrollY = 0
-  var ticking = false
-
-  function doSomething(scrollPosition) {
-    console.log('scrollPos', scrollPosition)
-  }
-
-  (function monkeyPatchWheelListener(window, document) {
+  ;(function monkeyPatchWheelListener(window, document) {
 
     var prefix = "", _addEventListener, support;
 
@@ -106,12 +99,13 @@ document.addEventListener('DOMContentLoaded', _ => {
   })(window, document)
 
   let deltaYSum = 0
+  let ticking = false
   window.addWheelListener(frame.el, evt => {
     deltaYSum += evt.deltaY
 
     if (!ticking) {
       window.requestAnimationFrame(_ => {
-        renderHandleDelta(handle, bar, deltaYSum)
+        scroll(frame, content, handle, bar, deltaYSum)
         ticking = false
         deltaYSum = 0
       })
@@ -119,23 +113,89 @@ document.addEventListener('DOMContentLoaded', _ => {
     ticking = true
   })
 
-  function getTopPosition(el) {
-    try {
-    return el.style.top === ''
-      ? 0
-      : parseInt(el.style.top.replace(/^(\d+)px$/, '$1'), 10)
-    } catch (_) {
-      return 0
-    }
+  function scroll(frame, content, handle, bar, deltaY) {
+    const newScrollTop = calcFrameScrollTop(frame, deltaY)
+    renderFrameScrollTop(frame, newScrollTop)
+    const percent = calcFramePercentScroll(frame, content)
+    const newHandleTop = calcHandleTop(bar, percent)
+    renderHandleTop(handle, newHandleTop)
   }
 
-  function renderHandleDelta(handle, bar, deltaY) {
-    const topWithDelta = getTopPosition(handle.el) + deltaY
-    const newTop = topWithDelta < 0
-      ? 0
-      : topWithDelta > bar.height - handle.height
-        ? bar.height - handle.height
-        : topWithDelta
-    handle.el.style.top = newTop + 'px'
+  function calcFrameScrollTop(frame, deltaY) {
+    return frame.el.scrollTop + deltaY
   }
+
+  function renderFrameScrollTop(frame, top) {
+    frame.el.scrollTop = top
+    return frame
+  }
+
+  function calcFramePercentScroll(frame, content) {
+    return frame.el.scrollTop / content.height
+  }
+
+  function calcHandleTop(bar, percent) {
+    return bar.height * percent
+  }
+
+  function renderHandleTop(handle, top) {
+    handle.el.style.top = top + 'px'
+    return handle
+  }
+
+  // function getTopPosition2(el) {
+  //   try {
+  //     return el.scrollTop === '' || el.scrollTop === 0
+  //       ? 0
+  //       : parseInt(el.style.top.replace(/^(\d+)px$/, '$1'), 10) || el.scrollTop
+  //   } catch (_) {
+  //     return 0
+  //   }
+  // }
+
+  // function renderContentDelta(frame, newTop) {
+  //   // const topWithDelta = getTopPosition2(frame.el) + deltaY
+  //   // const newTop = topWithDelta
+  //   // console.log('newTop', newTop)
+  //   // const newTop = topWithDelta < 0
+  //   //   ? 0
+  //   //   : topWithDelta > bar.height - handle.height
+  //   //     ? bar.height - handle.height
+  //   //     : topWithDelta
+  //   frame.el.scrollTop = newTop
+  // }
+
+  // function getTopPosition(el) {
+  //   try {
+  //     return el.style.top === ''
+  //       ? 0
+  //       : parseInt(el.style.top.replace(/^(\d+)px$/, '$1'), 10)
+  //   } catch (_) {
+  //     return 0
+  //   }
+  // }
+
+  // function renderHandleDelta(handle, bar, percentageScrolled) {
+  //   // TODO: move the bar down the percentage of the scrollTop / contentHeight
+  //   // const topWithDelta = getTopPosition(handle.el) + deltaY
+
+  //   const topBasedOnPercentScroll = percentageScrolled * bar.height - handle.height
+  //   console.log('topBasedOnPercentScroll', topBasedOnPercentScroll)
+
+  //   const newTop = topBasedOnPercentScroll < 0
+  //     ? 0
+  //     : topBasedOnPercentScroll > bar.height - handle.height
+  //       ? bar.height - handle.height
+  //       : topBasedOnPercentScroll
+  //   handle.el.style.top = newTop + 'px'
+  // }
 })
+
+/*
+
+ Capture mousewheel deltaY
+ scroll frame to deltaY
+ capture the scrollTop percent down the height of the content
+ move scrollbar handle to that percent down the bar
+
+ */
